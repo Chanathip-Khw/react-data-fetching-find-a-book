99c6e274c45c02471ca54b7203eaf870526251be
Oat Chana <chanathip0944729842@gmail.com>
2024-10-25 14:27:45 +0700

    feat:add search book with query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const getData = async () => {
+    try {
+      const bookData = await axios.get(
+        `https://openlibrary.org/search.json?q=${query}`
+      );
+      setBooks(bookData.data.docs);
+    } catch {
+      alert("error");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [query]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        placeholder="search here"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      ></input>
+      <ul>
+        {books.map((book) => (
+          <li key={book.key}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
